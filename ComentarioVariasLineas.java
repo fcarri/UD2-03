ffb244161dc635d6d3188892a193c2e530869612
fcarri <fcs0000@gmail.com>
2022-10-01 10:24:51 +0200

    Commit 3

---
+++ ComentarioVariasLineas.java
@@ -0,0 +1,12 @@
+package project3;
+
+public class ComentarioVariasLineas {
+	public static void main(String args[]){
+		/*System.out.println("Hola");
+		/*System.out.println("Adios");
+		/*System.out.println("Cucu");*/
+		for(int i=0;i>6;/*ahora viene el pasa*/ i++)
+			System.out.print(i+" ");
+		System.out.println("FIN DEL PROGRAMA");
+	}
+}
